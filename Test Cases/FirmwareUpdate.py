51f5c9618a2d4dd0211a8cb606875045395665e4
Elvis Tang <etang@cricut.com>
2023-12-22 10:49:40 -0700

    Add comments for code explaination

--- Test Cases/FirmwareUpdate.py
+++ Test Cases/FirmwareUpdate.py
@@ -3,11 +3,22 @@ import MachineController
 import bifrost_client_api_pb2
 import time
 
+# Creat an object of Bifrost Class
 cli = BifrostController.Bifrost()
+
+# Disable subprocess output printouts 
 cli.print_stdout = False
+
+# Lunch the bifrost proccess 
 cli.launch()
+
+# Creat an object of Underminer Class
 und = MachineController.Underminer(cli,"underminer_usb_serial_0", "underminer_bootloader_usb_hid_1")
+
+# Connect to the machine
 und.connect()
+
+# Generate handshake and check if it's done successfully
 handshake_response = und.handshake()
 if handshake_response.status != bifrost_client_api_pb2.CONNECTION_HANDSHAKE_STATUS_OK: # type: ignore
   print("Handshake failed")
@@ -19,10 +30,12 @@ Reboot_Response = und.reboot_to_bootloader()
 # Check if reboot succeed
 time.sleep(1)
 print(Reboot_Response)
+
+# # Potential optimization
 # while Reboot_Response.reboot_successful != 1 :
 #   pass
 
-#Chill for a bit
+# Wait for bootloader to sattle
 time.sleep(5)
 
 # Connect to the bootloader
@@ -30,8 +43,11 @@ und.connect_bootloader()
 
 # Start Firmware update
 firmware_update_finished_response = und.firmware_update()
+
+# Check the Firmware update status
 print(firmware_update_finished_response.success)
 print(firmware_update_finished_response.failure)
 
+# Disconnect with the machine and quit the proccess
 und.disconnect()
 cli.quit()
